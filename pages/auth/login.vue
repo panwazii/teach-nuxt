a996dff41599a03a5d7de5865ecdab04daa9414d
Panwa Muangsong <panwa1379@gmail.com>
2024-02-11 20:53:41 +0700

    init

---
+++ pages/auth/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="d-flex justify-center">
+    <v-card width="500" height="700">
+      <v-card-title> Login </v-card-title>
+      <v-card-text>
+        <v-text-field label="email" outlined></v-text-field>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Name : {{ form.name }}</h1>
+    <v-btn @click="changeName">btn</v-btn>
+    <h1>{{ showFullName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      fname: 'panwa',
+      lname: 'Mu',
+      age: 0,
+      form: { name: 'test', age: 0 },
+    }
+  },
+  methods: {
+    changeName() {
+      this.form.name = 'panwa'
+    },
+  },
+  computed: {
+    showFullName() {
+      return this.fname + ' ' + this.lname
+    },
+  },
+}
+</script>
